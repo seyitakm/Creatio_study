7eb209054618138722908230dc75eef9d2779760
Seyit <seyit200020@gmail.com>
2025-08-25 16:16:55 +0600

    Realty app added

---
+++ UsrMyDevPackage/SqlScripts/DeleteCarBodyTypeUniversalMSSQL/DeleteCarBodyTypeUniversalMSSQL.sql
@@ -0,0 +1,2 @@
+delete from UsrCarBodyType
+where id = '06b7f56e-e4e0-4af3-823e-a8bf19490d02'-- "Universal"
---
+++ UsrMyDevPackage/SqlScripts/DeleteUniversalCarBodyTypePostgreSql/DeleteUniversalCarBodyTypePostgreSql.sql
@@ -0,0 +1,2 @@
+delete from "UsrCarBodyType"
+where "Id" = '06b7f56e-e4e0-4af3-823e-a8bf19490d02' -- "Universal car body type"
