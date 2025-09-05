3f90ca2e1b38a7a9ff46848b2dd2a0c289a26ea9
Seyit <seyit200020@gmail.com>
2025-09-05 10:24:22 +0600

    Tournament section customized, added lookup

--- UsrTournament/Schemas/UsrTournament_ListPage/UsrTournament_ListPage.js
+++ UsrTournament/Schemas/UsrTournament_ListPage/UsrTournament_ListPage.js
@@ -3,32 +3,6 @@ define("UsrTournament_ListPage", /**SCHEMA_DEPS*/[]/**SCHEMA_DEPS*/, function/**
 		viewConfigDiff: /**SCHEMA_VIEW_CONFIG_DIFF*/[
 			{
 				"operation": "merge",
-				"name": "DataTable",
-				"values": {
-					"columns": [
-						{
-							"id": "f252f581-0ccf-44ac-b7c9-c00df2ad9919",
-							"code": "PDS_UsrName",
-							"caption": "#ResourceString(PDS_UsrName)#",
-							"dataValueType": 1
-						},
-						{
-							"id": "c8689d78-80ba-4e71-8cf2-fa478e3be5bc",
-							"code": "PDS_CreatedOn",
-							"caption": "#ResourceString(PDS_CreatedOn)#",
-							"dataValueType": 7
-						},
-						{
-							"id": "fd4b3485-a46e-4219-b775-adef1210fe51",
-							"code": "PDS_CreatedBy",
-							"caption": "#ResourceString(PDS_CreatedBy)#",
-							"dataValueType": 10
-						},
-					]
-				}
-			},
-			{
-				"operation": "merge",
 				"name": "MenuItem_ImportFromExcel",
 				"values": {
 					"clicked": {
@@ -40,11 +14,91 @@ define("UsrTournament_ListPage", /**SCHEMA_DEPS*/[]/**SCHEMA_DEPS*/, function/**
 				}
 			},
 			{
+				"operation": "remove",
+				"name": "SearchFilter",
+				"properties": [
+					"targetAttributes"
+				]
+			},
+			{
+				"operation": "merge",
+				"name": "SearchFilter",
+				"values": {
+					"_filterOptions": {
+						"expose": [
+							{
+								"attribute": "SearchFilter_Items",
+								"converters": [
+									{
+										"converter": "crt.SearchFilterAttributeConverter",
+										"args": [
+											"Items"
+										]
+									}
+								]
+							}
+						],
+						"from": [
+							"SearchFilter_SearchValue",
+							"SearchFilter_FilteredColumnsGroups"
+						]
+					}
+				}
+			},
+			{
 				"operation": "merge",
 				"name": "FolderTree",
 				"values": {
-					"sourceSchemaName": "FolderTree",
 					"rootSchemaName": "UsrTournament"
+				}
+			},
+			{
+				"operation": "merge",
+				"name": "DataTable",
+				"values": {
+					"columns": [
+						{
+							"id": "76fd1397-cb79-9a00-0fcc-e846969946ab",
+							"code": "PDS_UsrName",
+							"caption": "#ResourceString(PDS_UsrName)#",
+							"dataValueType": 27,
+							"width": 181.98843383789062
+						},
+						{
+							"id": "c8689d78-80ba-4e71-8cf2-fa478e3be5bc",
+							"code": "PDS_CreatedOn",
+							"caption": "#ResourceString(PDS_CreatedOn)#",
+							"dataValueType": 7,
+							"width": 161.98843383789062
+						},
+						{
+							"id": "d99670f5-d3ab-04bc-fee3-6439a0bb12d5",
+							"code": "PDS_UsrIsActive",
+							"caption": "#ResourceString(PDS_UsrIsActive)#",
+							"dataValueType": 12,
+							"width": 114
+						},
+						{
+							"id": "35e90291-9638-8070-b46e-cb2bbdc06bd8",
+							"code": "PDS_UsrCode",
+							"caption": "#ResourceString(PDS_UsrCode)#",
+							"dataValueType": 27,
+							"width": 114
+						},
+						{
+							"id": "060c570a-345d-d1a9-595e-98958052cfe4",
+							"code": "PDS_UsrManager",
+							"caption": "#ResourceString(PDS_UsrManager)#",
+							"dataValueType": 10,
+							"width": 127.98843383789062
+						},
+						{
+							"id": "ea8527bc-e985-8222-a70c-565b50faa5b4",
+							"code": "PDS_UsrTournamentType",
+							"caption": "#ResourceString(PDS_UsrTournamentType)#",
+							"dataValueType": 10
+						}
+					]
 				}
 			}
 		]/**SCHEMA_VIEW_CONFIG_DIFF*/,
@@ -68,11 +122,71 @@ define("UsrTournament_ListPage", /**SCHEMA_DEPS*/[]/**SCHEMA_DEPS*/, function/**
 							"path": "PDS.CreatedOn"
 						}
 					},
-					"PDS_CreatedBy": {
+					"PDS_UsrIsActive": {
 						"modelConfig": {
-							"path": "PDS.CreatedBy"
+							"path": "PDS.UsrIsActive"
+						}
+					},
+					"PDS_UsrCode": {
+						"modelConfig": {
+							"path": "PDS.UsrCode"
+						}
+					},
+					"PDS_UsrManager": {
+						"modelConfig": {
+							"path": "PDS.UsrManager"
+						}
+					},
+					"PDS_UsrTournamentType": {
+						"modelConfig": {
+							"path": "PDS.UsrTournamentType"
 						}
 					}
+				}
+			},
+			{
+				"operation": "merge",
+				"path": [
+					"attributes",
+					"Items",
+					"modelConfig"
+				],
+				"values": {
+					"filterAttributes": [
+						{
+							"loadOnChange": true,
+							"name": "FolderTree_active_folder_filter"
+						},
+						{
+							"name": "Items_PredefinedFilter",
+							"loadOnChange": true
+						},
+						{
+							"name": "LookupQuickFilterByTag_Items",
+							"loadOnChange": true
+						},
+						{
+							"name": "SearchFilter_Items",
+							"loadOnChange": true
+						}
+					]
+				}
+			},
+			{
+				"operation": "merge",
+				"path": [
+					"attributes",
+					"Items",
+					"modelConfig",
+					"sortingConfig"
+				],
+				"values": {
+					"default": [
+						{
+							"direction": "desc",
+							"columnName": "UsrName"
+						}
+					]
 				}
 			}
 		]/**SCHEMA_VIEW_MODEL_CONFIG_DIFF*/,
@@ -85,7 +199,27 @@ define("UsrTournament_ListPage", /**SCHEMA_DEPS*/[]/**SCHEMA_DEPS*/, function/**
 					"config"
 				],
 				"values": {
-					"entitySchemaName": "UsrTournament"
+					"entitySchemaName": "UsrTournament",
+					"attributes": {
+						"UsrName": {
+							"path": "UsrName"
+						},
+						"CreatedOn": {
+							"path": "CreatedOn"
+						},
+						"UsrIsActive": {
+							"path": "UsrIsActive"
+						},
+						"UsrCode": {
+							"path": "UsrCode"
+						},
+						"UsrManager": {
+							"path": "UsrManager"
+						},
+						"UsrTournamentType": {
+							"path": "UsrTournamentType"
+						}
+					}
 				}
 			}
 		]/**SCHEMA_MODEL_CONFIG_DIFF*/,
